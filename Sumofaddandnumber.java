c3dd48e2874907a0e267127bd02d34adbcfe5bcf
pavithra-6 <paviikannan1996@gmail.com>
2017-07-19 02:49:30 -0700

    Add files via upload

---
+++ Sumofaddandnumber.java
@@ -0,0 +1,23 @@
+package guvi;
+
+public class Sumofaddandnumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n=15;
+int sum=0;
+for(int i=1;i<=15;i++){
+sum+=i;
+	}
+System.out.println(sum);
+		int k=45;
+		int sum1=0;
+		for(int l=15;l<=45;l++){
+			if(l%2!=0){
+				sum1=sum1+l;
+						}
+		}
+		System.out.println(sum1);
+	}
+
+}
